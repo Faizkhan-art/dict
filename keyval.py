1daf34667bcbac40c5ff423258a7cd94f902d197
Faiz Khan <yahyahkhan@gmail.com>
2026-09-17 17:33:31 +0530

    update keyval

---
+++ keyval.py
@@ -0,0 +1,14 @@
+student = {
+    "name": "Faiz",
+    "age": 22,
+    "course": "Python"
+}
+
+print(student)
+print(student["name"])
+
+student["city"] = "Delhi"
+print(student)
+
+student["age"] = 23
+print(student)
